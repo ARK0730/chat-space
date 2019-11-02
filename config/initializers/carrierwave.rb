badfea8fd1182a516b900bb624a93c25bc4cc746
ARK0730 <tk.ark0730@gmail.com>
2019-11-03 00:52:11 +0900

    deploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'arkbucket'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/arkbucket'
 end
